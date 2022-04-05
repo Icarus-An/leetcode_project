498092e5257c7b7793e27fc44aa4447d800ba4b4
Icarus-An <zhiyuan.an@foxmail.com>
2022-04-05 12:57:23 +0800

    a

--- two_pointers/42.trapping-rain-water.java
+++ two_pointers/42.trapping-rain-water.java
@@ -7,8 +7,26 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        
+        // init variables
+        int i = 1, result = 0;
+        while (i < height.length - 1) {
+            int j = 0, tempRight = 0, tempLeft = 0, temp = 0;
+            while (j < i) {
+                tempLeft = Math.max(height[j], tempLeft);
+                j++;
+            }
+            j++;
+            while (j <= height.length - 1) {
+                tempRight = Math.max(height[j], tempRight);
+                j++;
+            }
+            temp = Math.min(tempLeft, tempRight) - height[i];
+            if (temp > 0) {
+                result += temp;
+            }
+            i++;
+        }
+        return result;
     }
 }
 // @lc code=end
-
