84b193eb74f5c8baa5bcc2c395bc05f7bba185bf
Icarus-An <zhiyuan.an@foxmail.com>
2022-04-20 22:31:44 +0800

    378 new solution

--- binary_search/378.kth-smallest-element-in-a-sorted-matrix.java
+++ binary_search/378.kth-smallest-element-in-a-sorted-matrix.java
@@ -10,16 +10,28 @@ package binary_search;
 
 // Binary Search plus Two Pointers
 class Solution {
+
+    public int myCount(int[][] matrix, int param) {
+        int len = matrix.length, count = 0, row = 0, col = len - 1;
+        while (row < len && col >= 0) {
+            if (matrix[row][col] <= param) {
+                count += col + 1;
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return count;
+    }
+
     public int kthSmallest(int[][] matrix, int k) {
         int l = matrix[0][0];
         int r = matrix[matrix.length - 1][matrix.length - 1];
 
         while (l < r) {
             int mid = l + (r - l) / 2;
-            int total = 0;
             // calculate the elements before the mid value
-            int i = 0, j = 0;
-            
+            int total = myCount(matrix, mid);
             if (total >= k) {
                 r = mid;
             } else {
@@ -28,5 +40,6 @@ class Solution {
         }
         return l;
     }
+    // Time complex: O(nlog(r-l))
 }
 // @lc code=end
